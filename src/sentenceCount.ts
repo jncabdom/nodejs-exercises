3429582f130c50277ec3dff14683972fffb64c92
Míriam Núñez García <mirinu97@gmail.com>
2021-11-10 19:48:55 +0000

    Ejercicio 4. Contar a's.

--- src/sentenceCount.ts
+++ src/sentenceCount.ts
@@ -1,8 +1,18 @@
-const sentence = 'Esta es una frase de prueba, que tiene 10 palabras';
+const sentence = 'Esta es una frase de prueba, que tiene 10 palabras aaaaaaa';
 
 function wordsCount(): number {
   const sentenceArray = sentence.split(' ');
   return sentenceArray.length;
 }
 
+function asCount(): number {
+  const count = sentence.match(/a/g);
+  if (count != null) {
+    return count.length;
+  } else {
+    return 0;
+  }
+}
+
 console.log(`The sentence '${sentence}' has ${wordsCount()} words`);
+console.log(`The sentence '${sentence}' has ${asCount()} a's`);
