6e1f9d7dbef6bf6a2197637b0333d8c9fa0dbf7c
Nicolás Cabrera <jncabdom@gmail.com>
2021-11-10 19:14:18 +0000

    Implemented solution for exercise 3.1 (counting words in a file)

---
+++ src/fileWordCount.ts
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const fileName = process.argv[2];
+
+fs.readFile(fileName, 'utf8', (err: Error, data: string) => {
+  if (err) {
+    console.log(err);
+  }
+  else {
+    console.log(data.replace(/\n/g, ' ')
+      .replace(/[&\/\\#,+()$~%.`'":*?<>{}]/g, ' ')
+      .replace(/  +/g, ' ')
+      .trim()
+      .split(' ').length);
+  }
+});
+
